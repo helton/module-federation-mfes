b48d00106ab33fa16e3af9946b981985ec7fa81e
Helton Carlos de Souza <heltoncarlossouza@gmail.com>
2022-06-15 03:26:55 -0300

    chore: setup

---
+++ remote1/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello World at { new Date().toString() }</h1>
+    /</div>
+  )
+}
---
+++ remote2/webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+    port: 3010,
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
+}
